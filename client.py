f2b5126364adb0bfb6146f31e705dbe5df343e6d
Otávio Pereira Tristão <78180132+Otavioo361@users.noreply.github.com>
2023-05-08 21:47:23 -0300

    ultima

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
 import Pyro4
-
 uri = input("Entre com a URI do servidor: ")
 username = input("Entre com o nome de usuário: ")
 password = input("Entre com a senha: ")
@@ -7,10 +6,14 @@ password = input("Entre com a senha: ")
 tts = Pyro4.Proxy(uri)
 
 if tts.authenticate(username, password):
-    text = input("Entre com o texto que deseja converter em áudio: ")
-    if tts.convert_text_to_speech(username, text):
-        print("Áudio gerado com sucesso!")
-    else:
-        print("Usuário não autenticado!")
+    while True:
+        text = input(
+            "Entre com o texto que deseja converter em áudio ou digite 'exit' para sair: ")
+        if text == "exit":
+            break
+        if tts.convert_text_to_speech(username, text):
+            print("Áudio gerado com sucesso!")
+        else:
+            print("Usuário não autenticado!")
 else:
     print("Falha na autenticação!")
--- server.py
+++ server.py
@@ -1,7 +1,25 @@
 import Pyro4
 import os
+import time
 import tempfile
 from gtts import gTTS
+import threading
+
+
+@Pyro4.expose
+class Request:
+    def __init__(self, username, text):
+        self.username = username
+        self.text = text
+
+    def process(self):
+        tts = gTTS(self.text, lang='pt-br')
+        tts.save('audio.mp3')
+        os.startfile('audio.mp3', 'play')
+        # linux
+        #os.system ('mpg321 audio.mp3')
+        time.sleep(1)
+        os.remove('audio.mp3')
 
 
 @Pyro4.expose
@@ -9,6 +27,10 @@ class TextToSpeech:
 
     def __init__(self):
         self.authenticated_users = set()
+        self.requests = []
+        self.request_lock = threading.Lock()
+        self.request_thread = threading.Thread(target=self.process_requests)
+        self.request_thread.start()
 
     def authenticate(self, username, password):
         # verifica se o nome de usuário e a senha fornecidos correspondem a um usuário válido
@@ -20,20 +42,29 @@ class TextToSpeech:
             return False
 
     def convert_text_to_speech(self, username, text):
-        
-
         # verifica se o usuário está autenticado
         if username in self.authenticated_users:
-            # converte o texto em áudio usando a biblioteca gTTS
-
-            tts = gTTS(text, lang='pt-br')
-            tts.save('audio.mp3')
-            os.system('mpg321 audio.mp3' )
-            os.remove('audio.mp3')
-            # reproduz o áudio usando o player
+            # cria um novo objeto Request com as informações do usuário e do texto
+            request = Request(username, text)
+            # adiciona o objeto Request à lista de solicitações
+            with self.request_lock:
+                self.requests.append(request)
             return True
         else:
             return False
+
+    def process_requests(self):
+        while True:
+            # verifica se há solicitações pendentes
+            with self.request_lock:
+                if len(self.requests) == 0:
+                    time.sleep(1)
+                    continue
+                # obtém a próxima solicitação na lista de solicitações
+                request = self.requests[0]
+                del self.requests[0]
+            # processa a solicitação
+            request.process()
 
 
 daemon = Pyro4.Daemon()
