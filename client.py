854454a55f28d1e19da3aa2536c50f2c7a72659d
Otávio Pereira Tristão <78180132+Otavioo361@users.noreply.github.com>
2023-05-08 23:04:28 -0300

    att f

--- client.py
+++ client.py
@@ -1,19 +1,31 @@
 import Pyro4
+import time
+
 uri = input("Entre com a URI do servidor: ")
 username = input("Entre com o nome de usuário: ")
 password = input("Entre com a senha: ")
 
-tts = Pyro4.Proxy(uri)
+tts = None
+for i in range(3):
+    try:
+        tts = Pyro4.Proxy(uri)
+        break
+    except:
+        print(f"Tentativa {i+1} de conexão falhou.")
+        time.sleep(5)
 
-if tts.authenticate(username, password):
-    while True:
-        text = input(
-            "Entre com o texto que deseja converter em áudio ou digite 'exit' para sair: ")
-        if text == "exit":
-            break
-        if tts.convert_text_to_speech(username, text):
-            print("Áudio gerado com sucesso!")
-        else:
-            print("Usuário não autenticado!")
+if tts is None:
+    print("Não foi possível conectar ao servidor.")
 else:
-    print("Falha na autenticação!")
+    if tts.authenticate(username, password):
+        while True:
+            text = input(
+                "Entre com o texto que deseja converter em áudio ou digite 'exit' para sair: ")
+            if text == "exit":
+                break
+            if tts.convert_text_to_speech(username, text):
+                print("Áudio gerado com sucesso!")
+            else:
+                print("Usuário não autenticado!")
+    else:
+        print("Falha na autenticação!")
--- server.py
+++ server.py
@@ -1,34 +1,46 @@
-import Pyro4
-import os
-import time
-import tempfile
+import Pyro4  # importa o módulo Pyro4 para criar um servidor de objetos distribuídos
+import os  # importa o módulo os para executar comandos do sistema operacional
+import time  # importa o módulo time para controlar o tempo de espera
+import tempfile  # importa o módulo tempfile para criar arquivos temporários
+# importa a classe gTTS da biblioteca gtts para converter texto em áudio
 from gtts import gTTS
-import threading
+import threading  # importa o módulo threading para gerenciar threads de execução concorrente
+
+# define uma classe para representar uma solicitação de conversão de texto em áudio
 
 
 @Pyro4.expose
 class Request:
     def __init__(self, username, text):
-        self.username = username
-        self.text = text
+        self.username = username  # nome de usuário que fez a solicitação
+        self.text = text  # texto a ser convertido em áudio
 
     def process(self):
+        # converte o texto em áudio usando a biblioteca gTTS
         tts = gTTS(self.text, lang='pt-br')
+        # salva o áudio em um arquivo temporário
         tts.save('audio.mp3')
+        # reproduz o áudio usando o player padrão do sistema operacional
         os.startfile('audio.mp3', 'play')
-        # linux
-        #os.system ('mpg321 audio.mp3')
+        # espera um segundo para garantir que o áudio seja reproduzido completamente antes de remover o arquivo temporário
         time.sleep(1)
+        # remove o arquivo temporário
         os.remove('audio.mp3')
+
+# define uma classe para representar o servidor de conversão de texto em áudio
 
 
 @Pyro4.expose
 class TextToSpeech:
 
     def __init__(self):
+        # armazena a lista de usuários autenticados
         self.authenticated_users = set()
+        # armazena a lista de solicitações pendentes de conversão de texto em áudio
         self.requests = []
+        # cria um objeto de bloqueio para garantir acesso exclusivo à lista de solicitações
         self.request_lock = threading.Lock()
+        # cria uma thread separada para processar as solicitações pendentes em segundo plano
         self.request_thread = threading.Thread(target=self.process_requests)
         self.request_thread.start()
 
@@ -36,6 +48,7 @@ class TextToSpeech:
         # verifica se o nome de usuário e a senha fornecidos correspondem a um usuário válido
         # retorna True se a autenticação for bem-sucedida, caso contrário, False
         if username == "otavio" and password == "otavio123":
+            # adiciona o usuário autenticado à lista de usuários autenticados
             self.authenticated_users.add(username)
             return True
         else:
@@ -46,7 +59,7 @@ class TextToSpeech:
         if username in self.authenticated_users:
             # cria um novo objeto Request com as informações do usuário e do texto
             request = Request(username, text)
-            # adiciona o objeto Request à lista de solicitações
+            # adiciona o objeto Request à lista de solicitações pendentes
             with self.request_lock:
                 self.requests.append(request)
             return True
@@ -54,6 +67,7 @@ class TextToSpeech:
             return False
 
     def process_requests(self):
+        # Executa um loop infinito para processar as solicitações de conversão de texto em fala.
         while True:
             # verifica se há solicitações pendentes
             with self.request_lock:
