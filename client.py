316c2a47d09eb28ded99f6514a40d23480356f5a
Otávio Pereira Tristão <otaviopere20@hotmail.com>
2023-05-12 22:38:38 -0300

    att final
    Att para autentificador

--- client.py
+++ client.py
@@ -1,16 +1,26 @@
 import Pyro4
 
-uri = input("Entre com a URI do servidor: ")
-username = input("Entre com o nome de usuário: ")
-password = input("Entre com a senha: ")
-
-tts = Pyro4.Proxy(uri)
-
-if tts.authenticate(username, password):
-    text = input("Entre com o texto que deseja converter em áudio: ")
-    if tts.convert_text_to_speech(username, text):
-        print("Áudio gerado com sucesso!")
+# Substitua pelo IP do servidor
+uri = "PYRO:server@{server_ip}:5050".format(server_ip="127.0.0.1")
+server = Pyro4.Proxy(uri)
+while True:
+    username = input("Digite o nome de usuário: ")
+    password = input("Digite a senha: ")
+    token = server.authenticate(username, password)
+    if token is not None:
+        break
     else:
-        print("Usuário não autenticado!")
-else:
-    print("Falha na autenticação!")
+        print("Usuário ou senha incorretos")
+
+while True:
+    message = input("Digite uma mensagem para enviar ao servidor,(exit) para finalizar: ")
+    while message == "":
+        message = input("Digite uma mensagem para enviar ao servidor,(exit) para finalizar: ")
+    if message != "exit":
+        try:
+            server.receive_messagem(message,token)
+        except Pyro4.errors.CommunicationError:
+            print("Erro: servidor fechado")
+            break
+    else:
+        break
--- server.py
+++ server.py
@@ -1,43 +1,74 @@
 import Pyro4
-import os
-import tempfile
+import socket
 from gtts import gTTS
+import os
+import threading
+import time
 
 
 @Pyro4.expose
-class TextToSpeech:
-
+class Server(object):
     def __init__(self):
-        self.authenticated_users = set()
-
-    def authenticate(self, username, password):
-        # verifica se o nome de usuário e a senha fornecidos correspondem a um usuário válido
-        # retorna True se a autenticação for bem-sucedida, caso contrário, False
-        if username == "otavio" and password == "otavio123":
-            self.authenticated_users.add(username)
-            return True
+        self.queue = []
+        self.lock = threading.Lock()
+        self.thread = threading.Thread(target=self.messagem)
+        self.thread.start()
+    def authenticate(self,username,password):
+        if username== "otavio" and password == "otavio123":
+            return "valid_token"
         else:
-            return False
+            return None
 
-    def convert_text_to_speech(self, username, text):
-        
 
-        # verifica se o usuário está autenticado
-        if username in self.authenticated_users:
-            # converte o texto em áudio usando a biblioteca gTTS
+    def messagem(self):
+        while True:
+            if len(self.queue) > 0:
+                messages = []
+                with self.lock:
+                    while len(self.queue) > 0:
+                        messages.append(self.queue.pop(0))
+                for message in messages:
+                    print("Mensagem rcebida: ", message)
+                    tts = gTTS(text=message, lang='pt-br')
+                    tts.save("audio.mp3")
+                    os.system("mpg321 audio.mp3")
+                    time.sleep(1)
+                    os.remove('audio.mp3')
 
-            tts = gTTS(text, lang='pt-br')
-            tts.save('audio.mp3')
-            os.system('mpg321 audio.mp3' )
-            os.remove('audio.mp3')
-            # reproduz o áudio usando o player
-            return True
+    def receive_messagem(self, message):
+        with self.lock:
+            self.queue.append(message)
+
+    def process(self):
+        while True:
+            with self.request:
+                if len(self.messagem) == 0:
+                    time.sleep(1)
+                    continue
+            messagem = self.messagem[0]
+            del self.messagem[0]
+            messagem.process()
+            
+    def receive_messagem(self, message, token):
+        if token == "valid_token":
+            with self.lock:
+                self.queue.append(message)
         else:
-            return False
+            print("Erro de autenticação")
 
 
-daemon = Pyro4.Daemon()
-tts = TextToSpeech()
-uri = daemon.register(tts)
-print(f"URI do servidor: {uri}")
+
+server = Server()
+
+# Crie um objeto Pyro4 daemon com o endereço IP da máquina e porta 9090
+daemon = Pyro4.Daemon(host="127.0.0.1", port=5050)
+
+# Registre o objeto Pyro4 com o endereço IP e porta do servidor
+uri = daemon.register(server, "server")
+ns = Pyro4.locateNS()
+ns.register("server", uri)
+print("Servidor registrado com sucesso em", uri)
+
+# Inicie o loop de eventos do Pyro4 daemon
+print("Aguardando conexões de clientes...")
 daemon.requestLoop()
